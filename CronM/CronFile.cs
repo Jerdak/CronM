1ca90124e89a979ec8f5f91f7490dcc2ca690e5c
jerdak_win7 <jerdak@gmail.com>
2011-06-01 09:54:46 -0400

    -  Updated crontab parser with some regular expressions. -  Added functionality for determining when the log was last modified (used by CronCore to reload file)

--- CronM/CronFile.cs
+++ CronM/CronFile.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
-
 namespace CronM
 {
 	/** Cron settings file
@@ -14,12 +13,23 @@ namespace CronM
 	*/
 	class CronFile
 	{
+		//Raw file
+		public FileInfo File { get; set; }
+
 		//Tasks
 		public List<CronTask> Tasks { get; set; }
 		
-		public CronFile()
+		//DateTime last modified
+		public DateTime LastModified { get; set; }
+
+		/**	Construtor
+		 *  @input
+		 *		fileName - Cron settings file name
+		*/
+		public CronFile(String fileName)
 		{
-			Tasks = new List<CronTask>();	
+			Tasks = new List<CronTask>();
+			File = new FileInfo(fileName);
 		}
 
 		///Utility function to remove inline |white space| > 1.
@@ -29,42 +39,55 @@ namespace CronM
 		}
 
 
-		public bool Load(String fileName)
+		public bool Load()
 		{
 			Tasks.Clear();
-
-			FileInfo file = new FileInfo(fileName);
-			if (!file.Exists) return false;
-
-			using (StreamReader sr = file.OpenText())
+			if (!File.Exists) return false;
+			
+			using (StreamReader sr = File.OpenText())
 			{
 				int uid = 1;
+				int lineCount = 1;
 				while (!sr.EndOfStream)
 				{
+					//Strip line of inline white space
 					String line = RemoveDuplicateWhiteSpace(sr.ReadLine().Trim());
-					String []tokens = line.Split(' ');
-
-					if (line.Length > 0 && line[0] != '#')
+					
+					//Match crontab format
+					Regex rgx = new Regex(@"\A(?:(?:[0-9*]+|,[0-9]+)+\s+){5}[a-zA-Z0-9\.]+\s+.+", RegexOptions.Singleline);
+					Match m = rgx.Match(line);
+					if (m.Success)
 					{
-						if (tokens.Length < 7)
+						try
 						{
-							Console.WriteLine("Warning, poorly formed cron line: %s\n", line);
-						}
-						else
-						{
+							String[] tokens = line.Split(' ');
 							CronTask task = new CronTask { Minute = tokens[0].Trim(), Hour = tokens[1].Trim(), DayOfMonth = tokens[2].Trim(), Month = tokens[3].Trim(), DayOfWeek = tokens[4].Trim(), User = tokens[5].Trim() };
 							StringBuilder command = new StringBuilder();
 							for (int i = 6; i < tokens.Length; i++)
 								command.Append(tokens[i].Trim() + ' ');
 							task.Command = command.ToString();
 							task.UniqueID = uid++;
-							task.LastRun = new DateTime(2010, 1, 1);	//arbitrary date set to some time at least 1 day or more back in time.  Allows first execution of a task that might be in the middle of its cycle
+							task.LastRun = new DateTime(2010, 1, 1);	//arbitrary date set to some time at least 1 day in time.  Allows first execution of a task that might be in the middle of a CronCore cycle
 							Tasks.Add(task);
 						}
+						catch (Exception ex)
+						{
+							Console.WriteLine("Warning, poorly formed crontab line[" + lineCount.ToString() + "]: " + line);
+						}
 					}
+					else
+					{
+						
+						if(line.Length >0)if(line[0] != '#')Console.WriteLine("Warning, poorly formed crontab line[" + lineCount.ToString() + "]: " + line);
+					}
+					lineCount++;
 				}
+				
 			}
-			return false;
+			//Last modified time is the last time this file was written to
+			LastModified = File.LastWriteTime;
+
+			return true;
 		}
 		public void DebugTasks()
 		{
